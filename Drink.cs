c405d7a606ec73b6040b20f067ef3eb3cb3efc12
Margarita Tkachenko <margosha2413@gmail.com>
2024-11-09 17:37:58 +0200

    Зміни в Drink

--- Drink.cs
+++ Drink.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Project
 {
-    internal class Drink
+    public class Drink : IOrderable
+    {
+        public string Name { get; }
+        public double Price { get; }
+        public DrinkType Type { get; }
+
+        public Drink(string name, double price, DrinkType type)
+        {
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+
+        public void PrintDetails()
     {
     }
 }
