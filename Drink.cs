9d731c8e486765f132abfe8bd2490864ea9b894c
Margarita Tkachenko <margosha2413@gmail.com>
2024-11-09 15:55:56 +0200

    Classes and interfase

--- Drink.cs
+++ Drink.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace Project
 {
-    public enum DrinkType
+    internal class Drink
     {
-        Coffee,
-        Tea,
-        Juice,
-        Water
     }
-
 }
--- IOrderable.cs
+++ IOrderable.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace Project
 {
-    public enum DrinkType
+    internal interface IOrderable
     {
-        Coffee,
-        Tea,
-        Juice,
-        Water
     }
-
 }
