d9eda58139d2650dabbd9c2555c580510feb7fa8
Margarita Tkachenko <margosha2413@gmail.com>
2024-11-23 13:56:26 +0200

    Зміни в Menu

--- Menu.cs
+++ Menu.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Project
 {
@@ -11,21 +7,36 @@ namespace Project
         public List<Dish> Dishes { get; } = new List<Dish>();
         public List<Drink> Drinks { get; } = new List<Drink>();
 
-        public void AddDish(Dish dish) => Dishes.Add(dish);
-        public void AddDrink(Drink drink) => Drinks.Add(drink);
+        public void AddDish(Dish dish)
+        {
+            Dishes.Add(dish);
+        }
+
+        public void AddDrink(Drink drink)
+        {
+            Drinks.Add(drink);
+        }
 
         public void PrintMenu()
         {
+            Console.WriteLine("=== Menu ===");
             Console.WriteLine("Dishes:");
-            foreach (var dish in Dishes) dish.PrintDetails();
+            foreach (var dish in Dishes)
+            {
+                dish.PrintDetails();
+            }
             Console.WriteLine("Drinks:");
-            foreach (var drink in Drinks) drink.PrintDetails();
+            foreach (var drink in Drinks)
+            {
+                drink.PrintDetails();
+            }
         }
 
         public void PrintReceipt()
         {
-            double total = Dishes.Sum(d => d.Price) + Drinks.Sum(d => d.Price);
-            Console.WriteLine($"Total: {total:C}");
+            Console.WriteLine("=== Receipt ===");
+            var total = Dishes.Sum(d => d.Price) + Drinks.Sum(d => d.Price);
+            Console.WriteLine($"Total: {total:F2}");
         }
     }
 }
