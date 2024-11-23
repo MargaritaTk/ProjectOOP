fabaf796a7b29724a9304864e5f95443df2bab1e
Margarita Tkachenko <margosha2413@gmail.com>
2024-11-23 13:56:03 +0200

    Абстрактний клас MenuItem

---
+++ MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project
+{
+    public abstract class MenuItem : IOrderable
+    {
+        public abstract string Name { get; set; }
+        public abstract double Price { get; set; }
+
+        public abstract void PrintDetails();
+    }
+}
