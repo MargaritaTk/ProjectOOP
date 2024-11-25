d75b276503300186eea077f1bc13efb76152371c
Margarita Tkachenko <margosha2413@gmail.com>
2024-11-25 10:29:57 +0200

    Drink зміни

--- Drink.cs
+++ Drink.cs
@@ -5,27 +5,15 @@ using System.Text;
 
 namespace Project
 {
-    public class Drink : MenuItem, ICloneable
+    public class Drink : IOrderable
     {
-        public override string Name { get; set; }
-        public override double Price { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
         public DrinkType Type { get; set; }
 
-        public override void PrintDetails()
+        public void PrintDetails()
         {
-            Console.WriteLine($"Drink: {Name}, Price: {Price:F2}, Type: {Type}");
-        }
-
-
-        public object Clone()
-        {
-            return new Drink
-            {
-                Name = this.Name,
-                Price = this.Price,
-                Type = this.Type
-            };
+            throw new NotImplementedException();
         }
     }
-
 }
