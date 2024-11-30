106830f91fd0de1bc4680a5d520009d8ff0d5f95
Margarita Tkachenko <margosha2413@gmail.com>
2024-11-30 21:11:29 +0200

    Зміни в Drink

--- Drink.cs
+++ Drink.cs
@@ -1,19 +1,34 @@
-﻿using System;
+﻿using Project;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
 namespace Project
 {
-    public class Drink : IOrderable
+    public class Drink : IComparable<Drink>, IOrderable
     {
         public string Name { get; set; }
         public double Price { get; set; }
         public DrinkType Type { get; set; }
 
+        public int CompareTo(Drink other)
+        {
+            if (other == null) return 1;
+
+            return this.Price.CompareTo(other.Price); 
+        }
+
+
         public void PrintDetails()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Drink: {Name}, Type: {Type}, Price: {Price} UAH");
+
         }
     }
 }
+    
+
+    
