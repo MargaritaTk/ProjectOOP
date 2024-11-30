6ab0d63dedc2fd3d51b7f15377472f05eab31ef5
Margarita Tkachenko <margosha2413@gmail.com>
2024-11-30 21:11:54 +0200

    Змінив в Dish

--- Dish.cs
+++ Dish.cs
@@ -1,16 +1,28 @@
-﻿using System;
+﻿using Project;
+using System;
 
 namespace Project
 {
-    public class Dish : IOrderable
+    public class Dish : IOrderable, IComparable<Dish>
     {
         public string Name { get; set; }
         public double Price { get; set; }
         public DishType Type { get; set; }
 
+        public int CompareTo(Dish other)
+        {
+            if (other == null) return 1;
+
+            return this.Price.CompareTo(other.Price); 
+        }
+
         public void PrintDetails()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Drink: {Name}, Type: {Type}, Price: {Price} UAH");
         }
     }
 }
+
+   
+
+    
