bde480223a3014f0a7683abab4b76032628b68b4
Margarita Tkachenko <margosha2413@gmail.com>
2024-11-30 18:23:12 +0200

    Доданий абстрактний клас

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public abstract class Person
+    {
+        public string Name { get; protected set; }
+
+        // Абстрактна властивість
+        public abstract string RoleDescription { get; set; }
+
+        protected Person(string name)
+        {
+            Name = name;
+        }
+
+        // Абстрактний метод
+        public abstract void Role();
+    }
+}
