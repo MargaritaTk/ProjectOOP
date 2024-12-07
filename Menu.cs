7fc0978190e258da2fd33b531653c80b848b0bd6
Margarita Tkachenko <margosha2413@gmail.com>
2024-12-07 19:11:30 +0200

    Зміни для вивода меню

--- Menu.cs
+++ Menu.cs
@@ -7,15 +7,26 @@ namespace Project
         public List<Dish> Dishes { get; } = new List<Dish>();
         public List<Drink> Drinks { get; } = new List<Drink>();
 
+        public delegate void MenuUpdated (string message);
+
+
+        public event MenuUpdated MenuUpdates; // Подія
+
         public void AddDish(Dish dish)
         {
             Dishes.Add(dish);
+            Dishes.Sort();
+            MenuUpdates?.Invoke($"Dish {dish.Name} added to the menu."); // Виклик події
         }
+
 
         public void AddDrink(Drink drink)
         {
             Drinks.Add(drink);
+            Drinks.Sort();
+            MenuUpdates?.Invoke($"Drink {drink.Name} added to the menu."); // Виклик події
         }
+
         public void SortDrinks()
         {
             Drinks.Sort();
@@ -25,19 +36,21 @@ namespace Project
             Dishes.Sort();
         }
 
-        public void PrintMenu()
+        public string PrintMenu()
         {
-            Console.WriteLine("=== Menu ===");
-            Console.WriteLine("Dishes:");
+            string menu = "=== Menu ===\nDishes:\n";
             foreach (var dish in Dishes)
             {
-                dish.PrintDetails();
+                menu += dish.PrintDetails() + "\n";
             }
-            Console.WriteLine("Drinks:");
+
+            menu += "Drinks:\n";
             foreach (var drink in Drinks)
             {
-                drink.PrintDetails();
+                menu += drink.PrintDetails() + "\n";
             }
+
+            return menu;
         }
     }
 }
